29de463e70450b62f2fd1f312015436625390203
IvashukR <rost87085@gmail.com>
2025-03-22 08:48:08 +0200

    cfg http

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ builder.Services.AddDb();
 builder.Services.AddScoped<PlayerRepo>();
 builder.Services.AddControllers();
 var app = builder.Build();
+app.Urls.Add("http://0.0.0.0:10000"); 
 app.UseRouting();
 app.UseEndpoints(endpoints => {
     endpoints.MapControllers();
